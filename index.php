9b4e6aa2dae55375c63927e795d064921a5ede08
Nicolas Fenwick <nicolas.fenwick@gmail.com>
2013-04-24 19:49:28 +1000

    First 2 section

---
+++ index.php
@@ -0,0 +1,58 @@
+<!doctype html>
+<html>
+<head>
+	<title>Publish What You Pay - Australia</title>
+	<script src='mis/js/jquery-1.7.1.min.js' type='text/javascript'></script>
+	<link rel="stylesheet" type="text/css" href="mis/css/main.css">
+</head>
+<body>
+	<nav id='nav-bar'>
+		<ul class='links'>
+			<li><a href='#resource-curse'>THE RESOURCE CURSE</a></li>
+			<li>|</li>
+			<li><a href='#pwyp'>ABOUT PWYP</a></li>
+			<li>|</li>
+			<li><a href='#act-now'>ACT NOW</a></li>
+		</ul>
+		<ul class='social'>
+			<li><a href='#'><span class='facebook'></span></a></li>
+			<li><a href='#'><span class='twitter'></span></a></li>
+		</ul>	
+		<div class='clear'></div>
+	</nav>
+	
+	<section id='mission'>
+		<div class='shadow'></div>
+		<div class='section-wrapper'>
+			<h1 class='jumbo'>
+				OUR RESOURCES,<br/>
+				OUR RIGHT TO KNOW
+			</h1>
+			<h3 class='logo'></h2>
+		</div>
+	</section>
+	
+	<section id='learn-more'>
+		<div class='top'></div>
+		<div class='section-wrapper'>
+			<div class='pull-right wider'>
+				<div class='title'>SPEAK OUT</div>
+				<div class='content'>Call on the Australian Government to act and introduce legislation requiring mining and oil and gas companies to publish what they pay to governments where they operate.</div>
+			</div>
+			<div class='pull-left'>
+				<div class='title'>LEARN MORE</div>
+				<div class='content'>Learn about the issue and how transparency can help end corruption and poverty in resource-rich countries. Download and read our booklet.</div>
+				<div class='booklet'>
+					<img src='mis/img/img-booklet1.jpg' alt='booklet'>
+					<img src='mis/img/img-booklet2.jpg' alt='booklet'>
+				</div>
+			</div>
+		</div>
+		<div class='bottom'></div>
+	</section>
+	
+	<section id='resource-curse'>
+		<div class='section-wrapper'></div>
+	</section>
+</body>
+</html>
