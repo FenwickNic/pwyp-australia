f4963f8ec95d88a360c922c4ce9d355b365131c5
Nicolas Fenwick <nicolas.fenwick@gmail.com>
2013-04-30 20:54:03 +1000

    Responsive

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
 <!doctype html>
 <html>
 <head>
-	<title>Publish What You Pay - Australia</title>
+	<title>Publish What You Pay Australia - Our resources, our right to know</title>
+	<meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
+	<meta name="description" content="Publish What You Pay is a global campaign for transparency and accountability in the mining and oil and gas industries. In Australia, the campaign is supported by a coalition of organisations and individuals that are committed to ensuring that citizens of resource-rich countries benefit from their natural wealth.">
+	<meta name="keywords" content="Mining, oil and gas, transparency, accountability, natural wealth">
+	<meta name="author" content="Nicolas Fenwick">
+	<meta name="robots" content="index, follow">
 	<script src='mis/js/jquery-1.7.1.min.js' type='text/javascript'></script>
+	<script src='mis/js/orangebox.min.js' type='text/javascript'></script>
 	<link rel="stylesheet" type="text/css" href="mis/css/main.css">
+	<link rel="stylesheet" type="text/css" href="mis/css/orangebox.css">
+	<!--[if IE 7]>
+		<link rel="stylesheet" media="all" href="mis/css/fontawesome-ie7.css" />
+	<![endif]-->
 </head>
 <body>
 	<nav id='nav-bar'>
@@ -20,7 +30,7 @@
 		</ul>	
 		<div class='clear'></div>
 	</nav>
-	<section id='mission'>
+	<div class='section' id='mission'>
 		<div class='shadow'></div>
 		<div class='section-wrapper'>
 			<h1 class='jumbo'>
@@ -29,10 +39,18 @@
 			</h1>
 			<h3 class='logo'></h3>
 		</div>
-	</section>	
-	<section id='learn-more'>
+	</div>	
+	<div class='section' id='learn-more'>
 		<div class='top'></div>
 		<div class='section-wrapper'>
+			<div class='pull-left'>
+				<div class='title'>LEARN MORE</div>
+				<div class='content'>Learn about the issue and how transparency can help end corruption and poverty in resource-rich countries. Download and read our booklet.</div>
+				<div class='booklet'>
+					<a href='mis/files/PWYP_booklet.pdf' data-ob="lightbox"><img src='mis/img/img-booklet1.jpg' alt='Read PWYP booklet'></a>
+					<a href='mis/files/PWYP_booklet.pdf' data-ob="lightbox"><img src='mis/img/img-booklet2.jpg' alt='Read PWYP booklet'></a>
+				</div>
+			</div>
 			<div class='pull-right wider'>
 				<div class='title'>SPEAK OUT</div>
 				<div class='content'>Call on the Australian Government to act and introduce legislation requiring mining and oil and gas companies to publish what they pay to governments where they operate.</div>
@@ -50,18 +68,11 @@
 					</form>
 				</div>
 			</div>
-			<div class='pull-left'>
-				<div class='title'>LEARN MORE</div>
-				<div class='content'>Learn about the issue and how transparency can help end corruption and poverty in resource-rich countries. Download and read our booklet.</div>
-				<div class='booklet'>
-					<a href='mis/files/PWYP_booklet.pdf'><img src='mis/img/img-booklet1.jpg' alt='booklet'></a>
-					<a href='mis/files/PWYP_booklet.pdf'><img src='mis/img/img-booklet2.jpg' alt='booklet'></a>
-				</div>
-			</div>
+			<div class='clear'></div>
 		</div>
 		<div class='bottom'></div>
-	</section>
-	<section id='resource-curse'>
+	</div>
+	<div class='section' id='resource-curse'>
 		<div class='section-wrapper'>
 			<div class='content'>
 				<div class='title'>THE RESOURCE CURSE</div>
@@ -71,14 +82,15 @@
 				</div>
 			</div>
 			<div class='globe'>
-				<div class='globe-mali'></div>
+				<div class='globe-mali globe-image'></div>
 				<div class='description'>
 					Residents of Sadiola village in Mali – like this young mother – have seen few benefits from the gold mine in their community. “Youth have no jobs and the men don’t have work at the mine,” says community leader Sora Sakiliba. “Thesedays, men usually have no money.”
 				</div>
 			</div>
+			<div class='clear'></div>
 		</div>
-	</section>
-	<section id='secrecy-and-corruption'>
+	</div>
+	<div class='section' id='secrecy-and-corruption'>
 		<div class='top'></div>
 		<div class='section-wrapper'>
 			<div class='content'>
@@ -86,9 +98,10 @@
 
 				<span class='pull-right'>
 					<strong>Campaigning for transparency in Niger</strong>
-					<a href='http://www.youtube.com/watch?v=LBqQQ3MVnBw'>
-						<div class='img-video-niger'>
-							<span class='description-hover' style='display:none'>This short film follows the activities of Ali Idrissa who coordinates the PWYP Niger coalition and their campaign for greater transparency and accountability in the oil, gas and mining industries. French with English subtitles.</span>
+					<a href='http://www.youtube.com/watch?v=LBqQQ3MVnBw' data-ob="lightbox">
+						<div class='video-wrapper'>
+							<div class='img-video-niger'></div>
+							<div class='description-hover'>This short film follows the activities of Ali Idrissa who coordinates the PWYP Niger coalition and their campaign for greater transparency and accountability in the oil, gas and mining industries. French with English subtitles.</div>
 						</div>
 					</a>
 				</span>
@@ -103,8 +116,8 @@
 				<span class='clear'></span>
 			</div>
 		</div>
-	</section>
-	<section id='continent-of-riches'>
+	</div>
+	<div class='section' id='continent-of-riches'>
 		<div class='section-wrapper'>
 			<div class='content'>
 				<div class='title'>
@@ -112,28 +125,30 @@
 				</div>
 				<div class='text'>Africa is experiencing a major expansion in natural resource extraction and currently around 240 Australian mining companies operate on the continent. In 2010 the exports of minerals and oil and gas were worth roughly seven times the amount given to the continent in aid. Yet without improved transparency this natural wealth is unlikely to be turned into lasting benefits.</div>
 			<span class='pull-right'>
-				<img src='mis/img/img-corruption.jpg'/>
+				<img src='mis/img/img-threequarter.png' alt="One quarter of Africa's income is lost because of corruption."/>
 			</span>
 			<span class='pull-left'>
-				<img src='mis/img/img-threequarter.jpg'/>
+				<img src='mis/img/img-corruption.png'/ alt="Corruption leads to infrastructure and hygiene issues.">
 			</span>
 			<span class='clear'></span>
-			
 			</div>
 		</div>
 		<div class='bottom'></div>
-	</section>
-	<section id='transparency-is-the-key'>
+	</div>
+	<div class='section' id='transparency-is-the-key'>
 		<div class='section-wrapper'>
-			<div class='content'>
+			<div class='content pull-right'>
 				<div class='title'>TRANSPARENCY IS THE KEY</div>
 				<div class='text'>
 					<p>Transparency of revenues from the sale of natural resources could help end corruption and ensure that this income is used for the public good. Thanks to pressure from Publish What You Pay, from the end of 2013 the United States followed by the European Union will require their mining and oil and gas companies to publish what they pay governments wherever they operate.</p>
 				</div>
 			</div>
+			<div class='clear'></div>
+			<div class='author'>Photo: Audrey Berry</div>
+			<div class='clear'></div>
 		</div>
-	</section>
-	<section id='time-for-australia-to-act'>
+	</div>
+	<div class='section' id='time-for-australia-to-act'>
 		<div class='section-wrapper'>
 			<div class='content'>
 				<div class='title'>TIME FOR AUSTRALIA TO ACT</div>
@@ -142,11 +157,19 @@
 					<p class='pull-right'>Mining taxes and royalties are often a topic of concern and controversy in Australia too. Greater transparency would help us understand the value of our nation’s natural resources.</p>
 					<div class='clear'></div>
 				</div>
+			</div>		
+			<div class='globe'>
+				<div class='description pull-right'>
+					The world's largest open pit gold mine, dubbed<br/>
+					'Super Pit', Kalgoorlie, Western Australia.<br/>
+					Photo: Shutterstock
+				</div>
+				<div class='globe-australia pull-left globe-image'></div>
 			</div>
-		
 		</div>
-	</section>
-	<section id='stand-with-us-and-speak-out'>
+	</div>
+	<div class='clear'></div>
+	<div class='section' id='stand-with-us-and-speak-out'>
 		<div class='section-wrapper'>
 			<div class='content'>
 				<div class='title'>STAND WITH US AND SPEAK OUT</div>
@@ -154,29 +177,90 @@
 					<p>Join the global movement for transparency to help end corruption and poverty in resource-rich countries and support the people’s right to know the value of their oil, gas and minerals.
 					</p><p>Call on the Australian Government to act and introduce legislation requiring mining and oil and gas companies to publish what they pay to governments where they operate. 
 					</p>
+					<div class='form'>
+					<form>
+						<div class='row'>
+							<input type='text' class='medium' name='firstname' value='' placeholder='First Name'></input>
+							<input type='text' class='medium' name='lastname' placeholder='Last Name'></input>
+						</div>
+						<div class='row'>
+							<input type='text' class='long' name='email' placeholder='Email'></input>
+							<input type='text' class='short' name='postcode' placeholder='Post Code'></input>
+						</div>
+						<div id='submit' class='btn'>Call on the Australian Government to <strong>act</strong><span class='chevron-right'></span></div>
+					</form>
+				</div>
+				</div>
+				<hr/>
+				<div class='social'>
+					<div class='pull-right'>
+						<span class='facebook'></span><span>BECOME A FAN ON FACEBOOK</span>
+					</div>
+					<div class='pull-left'>
+						<span class='twitter'></span><span>FOLLOW US ON TWITTER</span>
+					</div>
+					<div class='clear'></div>
 				</div>
 			</div>
-		
+			<div class='globe'>
+				<div class='globe-solomon globe-image'></div>
+				<div class='description'>
+					Evan Nancy and Pauline Tsira play downstream<br/>from the gold mine at Be Muta village in the<br/>Solomon Islands<br/>
+					Photo: Lara McKinley / Oxfam Australia.
+				</div>
+			</div>
 		</div>
-	</section>
-	<section id='about-pwyp'>
+	</div>
+	<div class='clear'></div>
+	<div class='section' id='about-pwyp'>
 		<div class='section-wrapper'>
 			<div class='fat-content'>
 				PUBLISH WHAT YOU PAY IS A GLOBAL CAMPAIGN FOR TRANSPARENCY AND ACCOUNTABILITY IN THE MINING AND OIL AND GAS INDUSTRIES. IN AUSTRALIA, THE CAMPAIGN IS SUPPORTED BY A COALITION OF ORGANISATIONS AND INDIVIDUALS THAT ARE COMMITTED TO ENSURING THAT CITIZENS OF RESOURCE-RICH COUNTRIES BENEFIT FROM THEIR NATURAL WEALTH.
-			</div>		
+			</div>	
+			<span class='clear'></span>
+		<div class='globe pull-right'>
+			<div class='globe-ghana globe-image'></div>
+			<div class='description'>
+				Godfried Ofori,  a member of the Concerned<br/> Citizens association of Presta, Ghana, looks out<br/> over an open pit at the Bogoso / Prestea Mine.<br/>
+				Photo: Jane Hahn/ Oxfam America.
+			</div>
+		</div>			
 		</div>
-	</section>
-	<section id='footer'>
+
+	</div>
+	<div class='clear'></div>
+	<div class='section' id='footer'>
 		<div class='top'></div>
+		<span class='pull-left logo-black'>
+		</span>
 		<span class='pull-right fat-content'>
 			For a list of member organisations, the latest news and resources please visit:<br/>
 			<a href='#' onclick='retrun false'>www.publishwhatyoupay.org/where/coalitions/australia</a>
 		</span>
-		<span class='pull-left'>
-			<h4 class='logo-black'></h4>
-		</span>
 		<span class='clear'>
 		</span>
-	</section>
+	</div>
+<script type='text/javascript'>
+	oB.settings.contentMaxSize = [1,1];
+$(document).ready(function(){
+
+	$('.video-wrapper').mouseenter(function(){
+		$('.description-hover').animate({top:'0px'});
+	}).mouseleave(function(){
+		$('.description-hover').animate({top:'274px'});
+	});
+	
+	$('.links a[href="#resource-curse"]').click(function(){
+		var height = $('#resource-curse').offset();
+		$('html, body').animate({scrollTop:height.top},'5000');	
+	});
+	$('.links a[href="#about-pwyp"]').click(function(){
+		var height = $('#about-pwyp').offset();
+		$('html, body').animate({scrollTop:height.top},'5000');	
+	});
+
+		
+});
+</script>
 </body>
 </html>
